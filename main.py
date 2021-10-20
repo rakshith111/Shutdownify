648cbc50d78d6a7293c45315106508890461fa5a
rakshith111 <rakshithbabu111@gmail.com>
2021-10-21 00:29:52 +0530

    Add extend feature
    Changed methods from system() to subprocess.run() for smoother working

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 from tkinter import *
-from os import system
 import tkinter as tk
+import subprocess
 def submit():
     try:
         hrsms=int(hrs.get())
         minuitsms=int(minuits.get())
         secondsms=int(minuits.get())
-        final=hrsms*3600+minuitsms*60+secondsms
         if((hrsms or minuitsms or secondsms)):
-            system(f"shutdown -s -t {final}")
+            final=hrsms*3600+minuitsms*60+secondsms
+            str="As"
+            subprocess.run(["shutdown", "-s", "-t" ,f"{final}"],  shell=True, stdout=subprocess.PIPE, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             hrs.set(int(0))
             minuits.set(int(0))
             seconds.set(int(0)) 
             cancel_btn.grid(row=2,sticky='e')
+            extend_btn.grid(row=2,sticky='w')
+        else:
+            pass
     except ValueError:
         hrs.set(int(0))
         minuits.set(int(0))
         seconds.set(int(0))
+def extend():
+    subprocess.run(["shutdown", "-a"],  shell=True, stdout=subprocess.PIPE, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    submit()
+
 def cancel():
     #if you want to cancel
-    system("shutdown -a")
+    subprocess.run(["shutdown", "-a"],  shell=True, stdout=subprocess.PIPE, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     root.destroy()
 root=tk.Tk()
 root.title('SHUTDOWN')
@@ -39,6 +47,7 @@ sec_label = tk.Label(root, text = 'Sec', font=('calibre',10, 'bold'))
 sec_entry = tk.Entry(root,textvariable = seconds, font=('calibre',10,'normal'))
 sub_btn=tk.Button(root,text = 'Submit', command = submit)
 cancel_btn=tk.Button(root,text="Cancel",command=cancel)
+extend_btn=tk.Button(root,text="EXTEND",command=extend)
 Hrs_label.grid(row=0,column=0)
 Hrs_entry.grid(row=1,column=0)
 min_label.grid(row=0,column=1)
