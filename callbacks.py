fcf736cbadc6448cabb59ba78d0b754723d9eada
rakshith111 <rakshithbabu111@gmail.com>
2022-08-28 23:40:04 +0530

    Update Screen timout

--- callbacks.py
+++ callbacks.py
@@ -1,6 +1,7 @@
 import subprocess
 from datetime import datetime
 import re
+from threading import Timer
 
 
 class callback():
@@ -8,25 +9,38 @@ class callback():
         current_power_scheme_cmd = subprocess.run(["powercfg ", "/GetActiveScheme"],  shell=False,
                                                   stdout=subprocess.PIPE, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-        current_power_scheme_guid = re.search(r'\w+-\w+-\w+-\w+-\w+',
-                                              str(current_power_scheme_cmd.stdout)).group()
+        self.current_power_scheme_guid = re.search(r'\w+-\w+-\w+-\w+-\w+',
+                                                   str(current_power_scheme_cmd.stdout)).group()
 
-        current_sleeper = subprocess.run(["powercfg", "/q",  f'{current_power_scheme_guid}', "SUB_VIDEO", "VIDEOIDLE"],  shell=False,
+        current_sleeper = subprocess.run(["powercfg", "/q",  f'{self.current_power_scheme_guid}', "SUB_VIDEO", "VIDEOIDLE"],  shell=False,
                                          stdout=subprocess.PIPE, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         searchhex = re.search(
             r'AC Power Setting Index:\s\w+', str(current_sleeper.stdout)).group()
         # int value of the AC power setting index
-        current_sleeper_seconds = int(searchhex[24:], 16)
+        self.current_sleeper_seconds = int(searchhex[24:], 16)
+        self.input_seconds = 0
+        print(self.current_sleeper_seconds)
+
+    def sleeper_action(self, state=True):
+        if state and self.input_seconds > 10:
+            print(f"Activated for {self.input_seconds-10} seconds")
+            # subprocess.run(["powercfg ", "/SETACVALUEINDEX", f'{self.current_power_scheme_guid}', "SUB_VIDEO", "VIDEOIDLE", "30"],  shell=False,
+            # stdout=subprocess.PIPE, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            t = Timer(self.input_seconds-10, self.sleep_timer)
+            t.start()
+
+    def sleep_timer(self):
+        print("restting")
         # TO set custom sleep time
-        subprocess.run(["powercfg ", "/SETACVALUEINDEX", f'{current_power_scheme_guid}', "SUB_VIDEO", "VIDEOIDLE", "800"],  shell=False,
-                       stdout=subprocess.PIPE, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        # subprocess.run(["powercfg ", "/SETACVALUEINDEX", f'{self.current_power_scheme_guid}', "SUB_VIDEO", "VIDEOIDLE", "self.current_sleeper_seconds"],  shell=False,
+        #   stdout = subprocess.PIPE, stdin = subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 
     def submit(self, seconds):
 
         self.base_time = datetime.now()
-
-        subprocess.run(["shutdown", "-s", "-t", f"{seconds}"],  shell=False,
+        self.input_seconds = seconds
+        subprocess.run(["shutdown", "-s", "-t", f"{self.input_seconds}"],  shell=False,
                        stdout=subprocess.PIPE, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     def extend(self, seconds):
@@ -38,8 +52,8 @@ class callback():
             subprocess.run(["shutdown", "-a"],  shell=False, stdout=subprocess.PIPE,
                            stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             self.elapsedtime = (datetime.now() - self.base_time).seconds
-
-            self.submit(self.elapsedtime+seconds)
+            self.input_seconds = self.elapsedtime+seconds
+            self.submit(self.input_seconds)
             return True
 
     def cancel(self):
